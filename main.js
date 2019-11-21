0d879b790ca0d41c9295000190d9aa7e5027aca7
Maran Filippo <filippo.maran.fm91@gmail.com>
2019-11-21 18:28:47 +0100

    fatto bonus, esercizio ok

--- main.js
+++ main.js
@@ -1,29 +1,60 @@
+// la difficoltà camvbia il numero max dell' intervallo da cui creare bombe e scegliere numeri
+var difficoltà = parseInt(prompt("Scegli la difficoltà, 0 = facile  1 = medio  2 = difficile"));
+var numeroMax = 0;
+switch(difficoltà) {
+    case 0:
+        numeroMax = 100;
+        break;
+    case 1:
+        numeroMax = 80;
+        break;
+    case 2:
+        numeroMax = 50;
+        break;
+    default:
+        numeroMax = 100;
+}
+// creao lista bombe
 var listaBombe = [];
-// var numero_iterazioni = 0;
 while(listaBombe.length < 16) {
-    var bomba = generaBombe(1, 100);
+    var bomba = generaBombe(1, numeroMax);
+    // ho usato la varibile creata sopra al posto del valore max fisso
     if(!listaBombe.includes(bomba)) {
         listaBombe.push(bomba);
     }
-    // numero_iterazioni++;
-    // console.log('numero iterazioni: ' + numero_iterazioni);
 }
 console.log(listaBombe);
-
+// funzione per creare numeri random per lista bombe
 function generaBombe(min, max) {
     var random = Math.floor(Math.random()* (max-min+1))+ min;
     return random;
 }
+// creo var che si incrementerà ad ogni ciclo per sapere quanti tentativi ci sono stati
+var numeroTentativi = 0;
 var esplosione = false;
-for (var i = 0; i < 84 && esplosione == false ; i++) {
-    var sceltaUtente = parseInt(prompt("Scrivi un numero da 1 a 100"));
-        if (listaBombe.includes(sceltaUtente)) {
-            esplosione = true;
-        }
+// utilizzo funzione per rendere più generico tentativiPossibili
+var tentativiPossibili = numeroMaxTentativi();
+// uso ciclo for perchè..mi piace controllarli, w la variabile sentinella
+for (var i = 0; i < tentativiPossibili && esplosione == false ; i++) {
+    var sceltaUtente = parseInt(prompt("Scrivi un numero da 1 a " + numeroMax ));
+    numeroTentativi++;
+    if (listaBombe.includes(sceltaUtente)) {
+        esplosione = true;
+        // se la variabile diventa true si ferma tutto
+    }
 }
-
+// funzione che rende più generica e scalibile il numero massimo di tentativi positivi che può fare l ' utente'
+function numeroMaxTentativi() {
+    var numero = numeroMax - listaBombe.length;
+    return numero;
+}
+// output a schermo
 if (esplosione) {
-    console.log("Hai perso");
+    console.log("Hai pestato una bomba, hai perso");
+    document.writeln("Hai pestato una bomba, hai perso....");
 } else {
-    console.log("hai vinto");
+    console.log("Hai appena usato tutta la fortuna che ti spettava in questa vita");
+    document.writeln("Hai appena usato tutta la fortuna che ti spettava in questa vita");
 }
+console.log("Numero passi fatti senza morire: " + (numeroTentativi - 1) );
+document.writeln("numero passi fatti senza morire: " + (numeroTentativi - 1));
